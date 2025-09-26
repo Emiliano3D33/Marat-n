8ef7d6c753d04835f35861613b017e6de5ba52e7
Your Name <ainaragomez379.ayg@gmail.com>
2025-09-25 21:48:08 -0300

    mi primer comit

--- src/persona.jsx
+++ src/persona.jsx
@@ -1,32 +1,47 @@
+import './Personas.css'
 export function Persona(){
     return(
-        <>
+        <> 
         <h1>THE FACTORY CHAMPIONS</h1>
-        <h2>¡Viví la experiencia de la Maratón!</h2>
+            <h2>¡Viví la experiencia de la Maratón!</h2>
+            <p>Sumate a uno de los eventos deportivos más esperados del año. 
+        Ya seas principiante o corredor experimentado, esta es tu oportunidad de desafiarte, 
+        compartir con otros y disfrutar de una jornada única.</p>
+         <h3>📅 Fecha y lugar</h3>
+                <ul>
+                    <li>Día: 23 de octubre de 2025</li>
+                    <li>Hora de largada: 10:00 A.M.</li>
+                    <li>Lugar: Parque Adán Quiroga</li>
+                </ul>
+            
+            
+                <h3>🏅 Distancias disponibles</h3>
+                <ul>
+                    <li>5K – Para los que recién empiezan.</li>
+                    <li>10K – Para quienes buscan superarse.</li>
+                    <li>21K y 42K – Para los más apasionados del running.</li>
+                </ul>
+            
 
-<p>Sumate a uno de los eventos deportivos más esperados del año. 
-Ya seas principiante o corredor experimentado, esta es tu oportunidad de desafiarte, 
-compartir con otros y disfrutar de una jornada única.</p>
-<p>📅 Fecha y lugar
-<br />Dia: 23 de octubre de 2025
-<br />Hora de largada: 10:00 A.M.
-<br />Lugar: Parque Adan Quiroga  </p>
-<p>🏅 Distancias disponibles
-<br />5K – Para los que recién empiezan.
-<br />10K – Para quienes buscan superarse.
-<br />21K y 42K – Para los más apasionados del running.</p>
-<p>🎁 Tu inscripción incluye
-<br />Remera oficial del evento.
-<br />Número de corredor 
-<br />Medalla finisher para todos los que crucen la meta.</p>
- <p>📍 Servicios en la carrera
-<br />Puestos de hidratación en el recorrido.
-<br />Asistencia médica y ambulancias.
-<br />Guardarropas y puestos de animación.
-
-<br />👉 No te quedes afuera. Los cupos son limitados.
-Hacé clic en el botón de inscripción, completá tus datos y asegurá tu lugar en la línea de largada.</p>
+                <h3>🎁 Tu inscripción incluye</h3>
+                <ul>
+                    <li>Remera oficial del evento.</li>
+                    <li>Número de corredor</li>
+                    <li>Medalla finisher para todos los que crucen la meta.</li>
+                </ul>
+          
+    
+                <h3>📍 Servicios en la carrera</h3>
+                <ul>
+                    <li>Puestos de hidratación en el recorrido.</li>
+                    <li>Asistencia médica y ambulancias.</li>
+                    <li>Guardarropas y puestos de animación.</li>
+                </ul>
+          
         
+            <p>👉 No te quedes afuera. Los cupos son limitados.</p>
+            <p>Hacé clic en el botón de inscripción, completá tus datos y asegurá tu lugar en la línea de largada.</p>
+            <a href="#" class="register-button">Inscríbete ahora</a>
         
         </>
     )
